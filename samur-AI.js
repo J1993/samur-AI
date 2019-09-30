4559d5d443879d14780400c56faeab302b9e2736
Jordy Lopez <jordizeus93@gmail.com>
2019-09-30 15:00:23 +0100

    consola

--- samur-AI.js
+++ samur-AI.js
@@ -5,24 +5,48 @@ const creador = {
     fecha_creacion : 26-09-2019,
     su : true
 }
-    
+
+/*Inicio de samu-AI*/
+init();
 
 function init() {
-    console.log(``)
+   consola();
+}
+
+/*Consola por la cual comunicarse con samur */
+function consola (){
+   bienvenida_random();
+}
+
+function ayuda () {
 }
 
 function bienvenida_random () {
-    var random_number = Math.floor((Math.random() * 10) + 1);
-    var respuesta
 
-    switch(random_number) {
-        case 1:
-          return respuesta = `Hola ${creador.nombre_creador}, en que puedo ayudarte hoy?`;
-          break;
-        case 2:
-          // code block
-          break;
-        default:
-          // code block
-      }
+   var random_number = Math.floor((Math.random() * 3) + 1);
+   var respuesta;
+
+  switch(random_number) {
+      case 1:
+         respuesta = `Hola ${creador.nombre_creador}, en que puedo ayudarte hoy?`;
+         break;
+      case 2:
+         respuesta = `Hola Sr.${creador.nombre_creador}, que podemos hacer hoy?`;
+         break;
+      
+      case 3:
+         respuesta = `Hola de nuevo Sr.${creador.nombre_creador}, que quieres saber?`;
+         break;
+      default:
+      // code block
+  }
+
+  var p = document.createElement("span");
+  var br = document.createElement("br");
+  var node = document.createTextNode(respuesta);
+  p.appendChild(node);
+  p.appendChild(br);
+
+  var consola = document.getElementById("consola");
+  consola.appendChild(p);
 }
