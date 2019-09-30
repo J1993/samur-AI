2a778b57cf457f9a7ec8b05fc7011593a1a17883
Jordy Lopez <jordizeus93@gmail.com>
2019-09-30 21:46:18 +0100

    Se incluye la ayuda de comandos.

--- samur-AI.js
+++ samur-AI.js
@@ -16,9 +16,24 @@ function init() {
 /*Consola por la cual comunicarse con samur */
 function consola (){
    bienvenida_random();
+   ayuda();
 }
 
-function ayuda () {
+function respuestas (respuesta) {
+
+   var respuesta;
+   this.respuesta = respuesta;
+
+   var span = document.createElement("span");
+   var br = document.createElement("br");
+   span.innerHTML = respuesta;
+   //var node = document.createTextNode(respuesta);
+   //span.appendChild(node);
+   span.appendChild(br);
+ 
+   var consola = document.getElementById("consola");
+   consola.appendChild(span);
+
 }
 
 function bienvenida_random () {
@@ -31,22 +46,35 @@ function bienvenida_random () {
          respuesta = `Hola ${creador.nombre_creador}, en que puedo ayudarte hoy?`;
          break;
       case 2:
-         respuesta = `Hola Sr.${creador.nombre_creador}, que podemos hacer hoy?`;
+         respuesta = `Hola Sr. ${creador.nombre_creador}, que podemos hacer hoy?`;
          break;
-      
       case 3:
-         respuesta = `Hola de nuevo Sr.${creador.nombre_creador}, que quieres saber?`;
+         respuesta = `Hola de nuevo Sr. ${creador.nombre_creador}, que quieres saber?`;
          break;
       default:
-      // code block
+         respuesta = `Vaya parece que ha habido un error.`;
+         break;
   }
 
-  var p = document.createElement("span");
-  var br = document.createElement("br");
-  var node = document.createTextNode(respuesta);
-  p.appendChild(node);
-  p.appendChild(br);
+  respuestas(respuesta);
+}
 
-  var consola = document.getElementById("consola");
-  consola.appendChild(p);
+function ayuda () {
+
+   var consola = document.getElementById("consola");
+   var a = document.createElement("a");
+   var br = document.createElement("br");
+   a.innerHTML = "Comandos";
+   a.appendChild(br);
+   a.href = "#";
+   a.setAttribute("onclick","mostrar_comandos()");
+ 
+   console.log(a);
+   consola.appendChild(a);
+}
+
+function mostrar_comandos() {
+   var comandos = "Estos son los diferentes comandos de Samur: <br> <br>" + 
+   " - Hola : Samur te saludara y dira quien es. ";
+   respuestas(comandos);
 }
